6be8284bb72c9ad05b56e069eabf665571d758dd
Azorgz <aurel.godet@gmail.com>
2026-08-27 14:21:21 +0200

    feat: add initial implementation of Depth Anything V2 with model configurations, dataset handling, and evaluation scripts

--- download_weights.sh
+++ download_weights.sh
@@ -2,6 +2,8 @@ cd model
 wget https://huggingface.co/spaces/LiheYoung/Depth-Anything/resolve/main/checkpoints_metric_depth/depth_anything_metric_depth_outdoor.pt -P backbone/Depth_anything/metric_depth/checkpoints
 wget https://huggingface.co/Azorgz/XCalib/resolve/main/checkpoint-10000.ckpt -P flow/CrossModalFlow/cross_raft_ckpt/model
 wget https://ml-site.cdn-apple.com/models/depth-pro/depth_pro.pt -P backbone/ml_depth_pro/checkpoints
+wget https://huggingface.co/depth-anything/Depth-Anything-V2-Metric-Hypersim-Small/resolve/main/depth_anything_v2_metric_hypersim_vits.pth?download=true -P /backbone/Depth_Anything_V2/metric_depth/checkpoints
+wget https://huggingface.co/depth-anything/Depth-Anything-V2-Metric-VKITTI-Small/resolve/main/depth_anything_v2_metric_vkitti_vits.pth?download=true -P /backbone/Depth_Anything_V2/metric_depth/checkpoints
 cd backbone/ml_depth_pro
 pip install -e .
 cd ../..
