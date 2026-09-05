ac3aca1a62fa399f219f786a42115e1a9964d55b
Azorgz <aurel.godet@gmail.com>
2026-09-05 10:53:45 +0200

    feat: update model loading path, add DA-2K evaluation benchmark, and modify configuration files

--- model/enhancer/__init__.py
+++ model/enhancer/__init__.py
@@ -4,9 +4,10 @@ import torch
 
 from model.enhancer.swinir_arch import Resnet
 
+ROOT = os.path.dirname(__file__)
 
 def get_enhancer():
     enhancer = Resnet()
-    checkpoint = torch.load(os.getcwd() + "/model/enhancer/checkpoint/enhancer_s.pth")
+    checkpoint = torch.load(ROOT + "/checkpoint/enhancer_s.pth")
     enhancer.load_state_dict(checkpoint, strict=False)
     return enhancer.eval()
